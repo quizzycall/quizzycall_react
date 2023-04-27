5ceaff92359adde5ef50cf96f3235f7b0d91c03a
Ivan Kriventsev <dirtyhornet277@gmail.com>
2023-04-27 10:00:55 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Home from "./components/pages/Home/Home"
 import Login from "./components/pages/Login/Login.jsx";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    // <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
